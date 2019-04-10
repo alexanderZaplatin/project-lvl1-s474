f71936eefe16bf5d26b2eb3865233a74072da844
Alexander <alexxspace@gmail.com>
2019-04-11 02:46:31 +0300

    correct func

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+
 const readlineSync = require('readline-sync');
+
 const getName = readlineSync.question('May I have your name?');
 console.log('Hi ' + getName + '!');
 
-export default getName
+export default getName;
