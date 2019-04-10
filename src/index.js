27fc6f2685dbea18f77f272608d0504601096a14
Alexander <alexxspace@gmail.com>
2019-04-10 04:03:50 +0300

    create index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const userName = () => {
+	const readlineSync = require('readline-sync');
+	const getName = readlineSync.question('May I have your name?');
+	console.log('Hi' + getName + '!');
+};
+export default userName;
