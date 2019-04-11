bffe9e424df21e326904724fe1ec8ba99328bfda
Alexander <alexxspace@gmail.com>
2019-04-11 12:11:02 +0300

    correct

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const userName = () => {
+  console.log('Welcome to the Brain Games!');
   const getName = readlineSync.question('May I have your name?');
   console.log(`Hi ${getName}!`);
 };
