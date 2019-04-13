ae9574173c3d7e451e720ed019f0506638b53a31
Alexander <alexxspace@gmail.com>
2019-04-14 02:25:40 +0300

    correct

--- src/games/even.js
+++ src/games/even.js
@@ -1,4 +1,4 @@
-import play from '../gameCore';
+import play from '..';
 import getRandomInt from '../tools/getRandomInt';
 
 const description = 'Answer "yes" if number even otherwise answer "no".';
