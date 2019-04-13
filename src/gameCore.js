42f45a5282dd66ae9fee179e4cecf253948681ac
Alexander <alexxspace@gmail.com>
2019-04-14 00:20:28 +0300

    correct func

--- src/gameCore.js
+++ src/gameCore.js
@@ -2,28 +2,27 @@ import readlineSync from 'readline-sync';
 
 const roundCount = 3;
 
-const game = (description, getGame) => {
+const play = (description, getDataGame) => {
   console.log('Welcome to the Brain Games!');
   console.log(`${description}`);
   const playerName = readlineSync.question('May I have your name?');
   console.log(`Hi ${playerName}!`);
 
   for (let i = 0; i < roundCount; i += 1) {
-    const [question, correctAnswer] = getGame();
+    const [question, correctAnswer] = getDataGame();
 
     console.log(`Question: ${question}`);
     const answerPlayer = readlineSync.question('Your answer: ');
 
-    if (answerPlayer !== String(correctAnswer)) {
-      console.log(`${answerPlayer} is wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${playerName}`);
+    if (answerPlayer === String(correctAnswer)) {
+      console.log('Correct!');
+    } else {
+      console.log(`${answerPlayer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
+      console.log(`Let's try again, ${playerName}`);
       return;
     }
-    if (answerPlayer === String(correctAnswer) && i === roundCount - 1) {
-      console.log(`Congratulations, ${playerName}!`);
-    } else {
-      console.log('Correct!');
-    }
   }
+  console.log(`Congratulations, ${playerName}!`);
 };
 
-export default game;
+export default play;
