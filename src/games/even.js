a3a5b9e4610ef16860053da3ff114b8e568371f6
Alexander <alexxspace@gmail.com>
2019-04-14 00:31:41 +0300

    change name func

--- src/games/even.js
+++ src/games/even.js
@@ -1,14 +1,14 @@
-import game from '../gameCore';
+import play from '../gameCore';
 import getRandomInt from '../tools/getRandomInt';
 
 const description = 'Answer "yes" if number even otherwise answer "no".';
 
 const isEven = num => num % 2 === 0;
 
-const getGame = () => {
+const getDataGame = () => {
   const question = getRandomInt(1, 100);
   const correctAnswer = isEven(question) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
-export default () => game(description, getGame);
+export default () => play(description, getGame);
