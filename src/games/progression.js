ad534ac05b000d2888778b9a851bb29bc403c552
Alexander <alexxspace@gmail.com>
2019-04-14 02:48:27 +0300

    add func progres

---
+++ src/games/progression.js
@@ -0,0 +1,32 @@
+import play from '..';
+import getRandomInt from '../tools/getRandomInt';
+
+const description = 'What number is missing in the progression?';
+const lengthProg = 10;
+const minRandom = 1;
+const maxRandom = 100;
+
+const getProgression = () => {
+  const progression = [];
+  const firstElement = getRandomInt(minRandom, maxRandom);
+  const step = getRandomInt(minRandom, maxRandom);
+
+  for (let i = 0; i < lengthProg; i += 1) {
+    const newElement = firstElement + (i * step);
+    progression.push(newElement);
+  }
+  return progression;
+};
+
+const getDataGeme = () => {
+  const progression = getProgression();
+  const indexPositionColon = getRandomInt(0, lengthProg - 1);
+
+  const correctAnswer = progression[indexPositionColon];
+  progression[indexPositionColon] = '..';
+  const question = String(progression.join(' '));
+
+  return [question, correctAnswer];
+};
+
+export default () => play(description, getDataGeme);
