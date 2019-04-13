b9524220ac4aa0d3b7a7223599ed8e99506d92bf
Alexander <alexxspace@gmail.com>
2019-04-14 02:21:30 +0300

    ref func

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,10 +3,8 @@ import getRandomInt from '../tools/getRandomInt';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
-const getMaxNum = (x, y) => (x > y ? x : y);
-
 const getGcd = (x, y) => {
-  const max = getMaxNum(x, y);
+  const max = x > y ? x : y;
   let div = 1;
   for (let i = 2; i < max; i += 1) {
     if (x % i === 0 && y % i === 0) {
